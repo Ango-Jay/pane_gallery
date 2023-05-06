4e60f55aac07ba142eab274fd9364a5e1075110c
Ango-Jay <angojeff433@gmail.com>
2023-05-06 16:35:17 +0100

    firebase config

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,15 @@ import Footer from "@/components/footer";
 import { GetImages } from "@/lib/pixabay";
 import { Image } from "@/interfaces";
 import classnames from "classnames";
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
+
+
 
 
 interface HomePageProps {
   initialImages: Image[];
 }
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBLAF3KO2cn-ai68-z6hQ7Vms7Wgn1DMdQ",
-  authDomain: "panegallery-7975c.firebaseapp.com",
-  projectId: "panegallery-7975c",
-  storageBucket: "panegallery-7975c.appspot.com",
-  messagingSenderId: "158047793157",
-  appId: "1:158047793157:web:d4576430989b398156ec6c",
-  measurementId: "G-CHV4E51P9J"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export default function Home({ initialImages }: HomePageProps) {
   const [darkTheme, setDarkTheme] = useState(false);
